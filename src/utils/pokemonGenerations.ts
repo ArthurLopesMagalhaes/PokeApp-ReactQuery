2080967edcb571f373b52bad1d037bb204353a60
Arthur Lopes Magalhães <arthurlopes462@hotmail.com>
2023-03-19 15:02:43 -0300

    :sparkles: feat: create pokemon generations helper.ts

---
+++ src/utils/pokemonGenerations.ts
@@ -0,0 +1,19 @@
+import Generation1Png from "../assets/pngs/generation_1.png";
+import Generation2Png from "../assets/pngs/generation_2.png";
+import Generation3Png from "../assets/pngs/generation_3.png";
+import Generation4Png from "../assets/pngs/generation_4.png";
+import Generation5Png from "../assets/pngs/generation_5.png";
+import Generation6Png from "../assets/pngs/generation_6.png";
+import Generation7Png from "../assets/pngs/generation_7.png";
+import Generation8Png from "../assets/pngs/generation_8.png";
+
+export const pokemonGenerations = [
+  { generation: "1", image: Generation1Png, text: "Generation I" },
+  { generation: "2", image: Generation2Png, text: "Generation II" },
+  { generation: "3", image: Generation3Png, text: "Generation III" },
+  { generation: "4", image: Generation4Png, text: "Generation IV" },
+  { generation: "5", image: Generation5Png, text: "Generation V " },
+  { generation: "6", image: Generation6Png, text: "Generation VI" },
+  { generation: "7", image: Generation7Png, text: "Generation VII" },
+  { generation: "8", image: Generation8Png, text: "Generation VIII" },
+];
