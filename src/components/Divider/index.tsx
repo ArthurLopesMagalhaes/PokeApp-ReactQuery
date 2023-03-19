48cd556d7afbe125b9c5ed48b6084bf768ca7202
Arthur Lopes Magalhães <arthurlopes462@hotmail.com>
2023-03-19 14:57:49 -0300

    :recycle: add 'left' and 'right' props to  the Divider component

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -3,8 +3,10 @@ import { Container } from "./styles";
 export interface IDivider {
   top?: number;
   bottom?: number;
+  left?: number;
+  right?: number;
 }
 
-export const Divider = ({ top, bottom }: IDivider) => {
-  return <Container top={top} bottom={bottom} />;
+export const Divider = ({ top, bottom, left, right }: IDivider) => {
+  return <Container top={top} bottom={bottom} left={left} right={right} />;
 };
--- src/components/Divider/styles.ts
+++ src/components/Divider/styles.ts
@@ -4,4 +4,6 @@ import { IDivider } from ".";
 export const Container = styled.View<IDivider>`
   margin-top: ${(props) => (props.top ? props.top : 0)}px;
   margin-bottom: ${(props) => (props.bottom ? props.bottom : 0)}px;
+  margin-left: ${(props) => (props.left ? props.left : 0)}px;
+  margin-right: ${(props) => (props.right ? props.right : 0)}px;
 `;
