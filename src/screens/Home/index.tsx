f867bbc1d3238d9088029ae1c90380f92bc15f44
Arthur Lopes Magalhães <arthurlopes462@hotmail.com>
2023-04-02 08:27:14 -0300

    ♻:recycle: change the menu icons

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,49 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList } from "react-native";
 import { Background } from "../../components/Background";
 import { Container, Description, FilterContainer, Title } from "./styles";
 
-import Vector from "../../assets/svgs/generation.svg";
-import Vector1 from "../../assets/svgs/sort.svg";
-import Vector2 from "../../assets/svgs/filter.svg";
+import GenerationSvg from "../../assets/svgs/generation.svg";
+import SortSvg from "../../assets/svgs/sort.svg";
+import FilterSvg from "../../assets/svgs/filter.svg";
 import { Divider } from "../../components/Divider";
 import { Input } from "../../components/Input";
 import { PokemonCard } from "../../components/PokemonCard";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { FilterBottomSheet } from "../../components/FilterBottomSheet";
+import { useTheme } from "styled-components";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { GenerationBottomSheet } from "../../components/GenerationBottomSheet";
+import { SortBottomSheet } from "../../components/SortBottomSheet";
+import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
 
 export const Home = () => {
+  const theme = useTheme();
+  const generationSheetRef = useRef<BottomSheet>(null);
+  const sortSheetRef = useRef<BottomSheet>(null);
+  const filterSheetRef = useRef<BottomSheet>(null);
+
+  const openModal = (bottomSheet: React.RefObject<BottomSheetMethods>) => {
+    bottomSheet.current?.snapToIndex(0);
+  };
+
   const [pokemons, setPokemons] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
 
   return (
     <Background>
       <Container>
         <FilterContainer>
-          <Vector fill={"yellow"} />
-          <Vector1 />
-          <Vector2 />
+          <GenerationSvg
+            color={theme.colors.textBlack}
+            onPress={() => openModal(generationSheetRef)}
+          />
+          <SortSvg
+            color={theme.colors.textBlack}
+            onPress={() => openModal(sortSheetRef)}
+          />
+          <FilterSvg
+            color={theme.colors.textBlack}
+            onPress={() => openModal(filterSheetRef)}
+          />
         </FilterContainer>
 
         <Divider top={35} />
@@ -41,12 +65,15 @@ export const Home = () => {
         <FlatList
           data={pokemons}
           keyExtractor={(item) => item.toString()}
-          renderItem={({ item }) => <PokemonCard />}
           contentContainerStyle={{ paddingBottom: 70, paddingTop: 20 }}
           showsVerticalScrollIndicator={false}
           ItemSeparatorComponent={() => <Divider top={30} />}
+          renderItem={({ item }) => <PokemonCard onPress={() => null} />}
         />
       </Container>
+      <GenerationBottomSheet ref={generationSheetRef} />
+      <SortBottomSheet ref={sortSheetRef} />
+      <FilterBottomSheet ref={filterSheetRef} />
     </Background>
   );
 };
