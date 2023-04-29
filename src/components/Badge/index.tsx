f9eb3c0601aafda4894fb0b107a6f74b4509d80f
Arthur Lopes Magalhães <arthurlopes462@hotmail.com>
2023-04-29 17:14:05 -0300

    :lipstick: update margin when dosent have any label

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -3,13 +3,16 @@ import { Container, IBadgeStyle, Text } from "./styles";
 
 interface IBadge {
   icon: React.FC<SvgProps>;
-  label: string;
+  label?: string;
   stylesProps: IBadgeStyle;
 }
 
 export const Badge = ({ icon: Icon, label, stylesProps }: IBadge) => {
   return (
-    <Container backgroundColor={stylesProps.backgroundColor}>
+    <Container
+      backgroundColor={stylesProps.backgroundColor}
+      label={label ? true : false}
+    >
       <Icon height={20} color={"#FFF"} />
       <Text>{label}</Text>
     </Container>
--- src/components/Badge/styles.ts
+++ src/components/Badge/styles.ts
@@ -2,12 +2,13 @@ import styled from "styled-components/native";
 
 export interface IBadgeStyle {
   backgroundColor: string;
+  label: boolean;
 }
 
 export const Container = styled.View<IBadgeStyle>`
   background-color: ${(props) => props.backgroundColor};
   padding: 5px;
-  gap: 5px;
+  gap: ${(props) => (props.label ? "5px" : 0)};
   border-radius: 3px;
   flex-direction: row;
   align-items: center;
